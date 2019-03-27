81249a3e170c975a4c655343a01aa081e013f39d
cxmgit <458225467@qq.com>
2019-03-27 21:27:02 +0800

    修改分支代码

--- javagitdemo/src/demo.java
+++ javagitdemo/src/demo.java
@@ -5,8 +5,8 @@ public class demo {
     public static void main(String[] args) {
         //这是我第一次使用git
         //我在测试哦
-        System.out.println("分支");
-        System.out.println("第二次修改");
+
+        System.out.println("切换到分支");
 
 
     }
