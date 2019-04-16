768cbc03b890f032a443652576fc7efa5ca0c192
cxmgit <458225467@qq.com>
2019-04-16 22:56:17 +0800

    只提交不上传

---
+++ javagitdemo/src/CopyBeanTest.java
@@ -0,0 +1,13 @@
+import pojo.User;
+import pojo.UserVO;
+
+/**
+ * Created by Administrator on 2019/3/31/031.
+ */
+public class CopyBeanTest {
+    public static void main(String[] args) {
+        User user = new User("张三",18);
+        UserVO userVO = new UserVO();
+
+    }
+}
