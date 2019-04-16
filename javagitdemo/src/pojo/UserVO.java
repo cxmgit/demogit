f05af1b9e2aa8a1a1e179eac9e84767567280e3e
cxmgit <458225467@qq.com>
2019-04-16 22:57:03 +0800

    POJO

---
+++ javagitdemo/src/pojo/UserVO.java
@@ -0,0 +1,19 @@
+package pojo;
+
+/**
+ * Created by Administrator on 2019/3/31/031.
+ */
+public class UserVO {
+    private  String namea;
+
+    public UserVO() {
+    }
+
+    public String getNamea() {
+        return namea;
+    }
+
+    public void setNamea(String namea) {
+        this.namea = namea;
+    }
+}
